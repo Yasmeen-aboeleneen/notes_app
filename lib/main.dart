a10214d3dec6671e33048a0d0cb3f9829a91b76d
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-04-10 08:24:53 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Notes());
+}
+
+class Notes extends StatelessWidget {
+  const Notes({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       
+      theme: ThemeData(
+         
+        primarySwatch: Colors.blue,
+      ),
+        
+    );
+  }
+}
+
+ 
+
+  
+
+  
