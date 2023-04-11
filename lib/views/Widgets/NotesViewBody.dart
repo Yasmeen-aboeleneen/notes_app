7d582b6935c3ba3e922d766d3eb827f1ab7f5275
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-04-12 01:39:23 +0200

    create Custom App Bar

--- lib/views/Widgets/NotesViewBody.dart
+++ lib/views/Widgets/NotesViewBody.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/Widgets/CustomAppBar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+       CustomAppBar()
+
+      ],
+    );
   }
 }
+ 
