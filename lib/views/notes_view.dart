15fb90a2ba3f73cdc99100365fff9b7ecbfc5002
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-04-12 01:33:34 +0200

    create Notes View body

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,21 @@
 
 import 'package:flutter/material.dart';
 
+import 'Widgets/NotesViewBody.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+
+              body: NotesViewBody(),
+
+
+
+    );
   }
 }
+
+ 
